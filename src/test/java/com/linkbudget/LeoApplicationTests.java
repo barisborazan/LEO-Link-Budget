695433ea0aaff8788a6d7c0128dcedaafd4cf3ff
barisborazan <baris_borazan@hotmail.com>
2025-03-02 13:34:26 +0300

    Initial commit

---
+++ src/test/java/com/linkbudget/LeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
